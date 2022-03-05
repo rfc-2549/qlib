8560523f815d691ac0fa33ebdaa5ebee3bf04905
qorg11 <nbome@riseup.net>
2022-03-05 18:36:37 +0100

    Use own get_rand_number() function for shuffle.c as it gives a better random number than rand() because it uses /dev/urandom instead of current time.

--- src/shuffle.c
+++ src/shuffle.c
@@ -2,19 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 #include <qlib/shuffle.h>
+#include <qlib/rand.h>
 
 void
 shuffle(void *base, size_t len, size_t nmemb)
 {
-	char *tmp = malloc(len*nmemb);
+	char *tmp = malloc(len * nmemb);
 	char *arr = base;
 	int stride = nmemb * sizeof(char);
 
 	if(len > 1) {
 		for(size_t i = 0; i < len - 1; i++) {
-			int j = i + rand() / (RAND_MAX / (len - i) + 1);
+			unsigned int x = get_rand_number();
+			unsigned int j = i + x / ( RAND_MAX / (len - i) + 1);
+			/* Sometimes j can be larger than the length, in this
+			 * case decrement the thing until it's a valid array index.
+			 */
+			while(len - 1 < j)
+				j--;
 			memcpy(tmp, arr + j * stride, nmemb);
 			memcpy(arr + j * stride, arr + i * stride, nmemb);
 			memcpy(arr + i * stride, tmp, nmemb);
@@ -27,14 +35,17 @@ shuffle(void *base, size_t len, size_t nmemb)
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int
-main(void) {
-	srand(time(0));
-	int x[5] = {1,2,3,4,5};
-	shuffle(x,5,sizeof(int));
-	for(int i = 0; i < 5; i++) {
-		printf("%i\n",x[i]);
+main(void)
+{
+	srand(time(NULL));
+	int x[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	shuffle(x, 8, sizeof(int));
+	puts("-----------");
+	for(int i = 0; i < 8; i++) {
+		printf("%i\n", x[i]);
 	}
 	return 0;
 }
-#endif  /* testing */
+#endif /* testing */
